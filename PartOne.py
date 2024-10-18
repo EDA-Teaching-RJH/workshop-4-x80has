0b9ab99ad280d3c64698b3bd6df80eb93338ba00
x80has <has36@kent.ac.uk>
2024-10-18 10:24:05 +0000

    finished

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,7 @@
-
+camel_case= input("Please enter the name of the variable in camel case: ")
+for i in camel_case:
+    if i.islower():
+     print(i, end="")
+    if i.isupper():
+        print ("_",i.lower(), end="")
+print()
--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,12 @@
+price= 75
+coins= [50,20,10,5]
+initial = 0
 
+while price>0:
+    pay= int(input("enter the coins insterted : "))
+    if pay in coins:
+        price=price-pay
+        initial =initial+pay
+
+if initial > price:
+    print(f"change owned : {initial-75}")
